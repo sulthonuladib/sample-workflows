8f122f4e06a9243dff2a9689331bb7b3a005bfe2
sulthonuladib <adibcyber4rt@gmail.com>
2022-05-10 18:24:33 +0700

    feat(rest): add simple hello endpoint, remove node_module

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 
+const helloRouter = require('./routes/hello.routes');
+
 const app = express();
+
+app.use('/hello', helloRouter);
 
 app.listen(3000, () => {
     console.log(`App is running on http://localhost:3000`);
