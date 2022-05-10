3cddcef18290ba804baf725abd4d01a7ad13cde4
adibatsprout <sulthonuladib@@sproutasia.com>
2022-05-10 19:13:00 +0700

    hotfix(root): test case pass

--- tests/root.test.js
+++ tests/root.test.js
@@ -4,6 +4,6 @@ const app = require('../src/app');
 describe('Test error for all routes', () => {
     test("It should not response GET request", async() => {
         const response = await request(app).get('/');
-        expect(response.statusCode).toBe(500);
+        expect(response.statusCode).toBe(404);
     });
 })
